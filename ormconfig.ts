5bda1d9458fa8509bfeac0a11c47f5c2deaa8d92
Daniel <danlpsarcanjo@gmail.com>
2022-03-08 21:29:18 -0500

    create createTypeOrmConfig function

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,3 +1,9 @@
-import { typeOrmConfigFactory } from './src/config/typeOrmConfig';
+import { createTypeOrmConfig } from './src/config/typeOrmConfig';
 
-export = typeOrmConfigFactory();
+export = createTypeOrmConfig({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
--- src/config/typeOrmConfig.ts
+++ src/config/typeOrmConfig.ts
@@ -2,31 +2,36 @@ import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from '../users/user.entity';
 
-const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'mysql',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  entities: [User],
-  migrations: ['migration/*.ts'],
-  cli: {
-    migrationsDir: 'migration',
-  },
+interface CreateTypeOrmConfigOptions {
+  host?: string;
+  port?: number;
+  username?: string;
+  password?: string;
+  database?: string;
+}
+
+export const createTypeOrmConfig = (options: CreateTypeOrmConfigOptions): TypeOrmModuleOptions => {
+  return {
+    type: 'mysql',
+    host: options.host,
+    port: options.port,
+    username: options.username,
+    password: options.password,
+    database: options.database,
+    entities: [User],
+    migrations: ['migration/*.ts'],
+    cli: {
+      migrationsDir: 'migration',
+    },
+  };
 };
 
-export const typeOrmConfigFactory = (configService?: ConfigService): TypeOrmModuleOptions => {
-  if (!configService) {
-    return typeOrmConfig;
-  }
-
-  return {
-    ...typeOrmConfig,
+export const typeOrmConfigFactory = (configService: ConfigService): TypeOrmModuleOptions => {
+  return createTypeOrmConfig({
     host: configService.get('DB_HOST'),
     port: configService.get('DB_PORT'),
     username: configService.get('DB_USERNAME'),
     password: configService.get<string>('DB_PASSWORD'),
     database: configService.get<string>('DB_DATABASE'),
-  };
+  });
 };
